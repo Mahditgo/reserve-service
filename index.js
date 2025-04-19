81adf4d894f2fb0ae3b11e2672dcc9c522dade73
Mahditgo <mahdi90056@gmail.com>
2025-04-19 23:07:09 +0330

    service section completed

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
-dotenv.config();
+const helmet = require('helmet');
+const cors =    require('cors');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
 
+
+dotenv.config();
 const app = require('./src/app');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+});
+
+app.use(helmet());
+app.use(cors());
+app.use(limiter);
+app.use(xss());
+
 const conncectDB = require('./src/config/db.config');
 
 
